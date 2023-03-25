7de695a55f07ab7cbeccef14c8cdb801cf292ce9
wesley <rladhlthf123@naver.com>
2023-03-25 13:05:09 +0900

    0. 학습테스트 실습 추가

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,55 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void getSize() {
+        int size = numbers.size();
+        assertThat(size).isEqualTo(3);
+    }
+
+    @Test
+    void containsV1() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void containsV2(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    Boolean checkOverThree(int number) {
+        return number > 0 && number <= 3;
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:True", "2:True", "3:True", "4:False", "5:False"}, delimiter = ':')
+    void containsV2(int input, boolean tf) {
+        assertThat(checkOverThree(input)).isEqualTo(tf);
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,53 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
-    void replace() {
+    public void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    public void splitV1() {
+        String temp = "1,2";
+        List<String> splitList = Arrays.asList(temp.split(","));
+        assertThat(splitList).containsExactly("1", "2");
+    }
+
+    @Test
+    public void splitV2() {
+        String temp = "1";
+        List<String> splitList = Arrays.asList(temp.split(","));
+        assertThat(splitList).containsExactly("1");
+    }
+
+    @Test
+    public void splitV3() {
+        String temp = "(1,2)";
+        temp = temp.replace("(", "");
+        temp = temp.replace(")", "");
+        List<String> splitList = Arrays.asList(temp.split(","));
+        assertThat(splitList).containsExactly("1", "2");
+    }
+
+    @DisplayName("charAt 메서드 테스트")
+    @Test
+    public void charAtV1() {
+        // given
+        String actual = "abc";
+
+        // when, then
+        assertThatThrownBy(() -> actual.charAt(actual.length())).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: ")
+                .hasMessageContaining(String.valueOf(actual.length()));
     }
 }
